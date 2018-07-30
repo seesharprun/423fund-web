19bb23dd16f9117525ebde763f7b6646b5a2c576
Sidney Andrews <sidney@seesharprun.net>
2018-07-30 11:36:22 -0400

    Added donate button

---
+++ Pages/Donate/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _423fund.Pages.Donate
+{
+    public class IndexModel : PageModel
+    {
+        public double Amount { get; set; }
+
+        public void OnGet(double? amount)
+        {
+            Amount = amount ?? 50.00;
+        }
+    }
+}
